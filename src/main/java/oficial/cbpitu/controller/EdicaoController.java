1c6fd826881c8b15f8320dbf540302d1d2bc7ebb
CunhazadaNoDale <cunha12324@gmail.com>
2025-12-14 22:53:56 -0300

    fix: inclusão de /api nas chamadas

--- src/main/java/oficial/cbpitu/controller/EdicaoController.java
+++ src/main/java/oficial/cbpitu/controller/EdicaoController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/edicoes")
+@RequestMapping("/api/edicoes")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 public class EdicaoController {
--- src/main/java/oficial/cbpitu/controller/EscalacaoController.java
+++ src/main/java/oficial/cbpitu/controller/EscalacaoController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/escalacoes")
+@RequestMapping("/api/escalacoes")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 public class EscalacaoController {
